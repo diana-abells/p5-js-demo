b57330578ea4d7f22b372a12d61cd9e12cd94fd8
Diana <79723469+diana-abells@users.noreply.github.com>
2021-09-28 11:51:11 -0400

    Add files via upload

---
+++ sketches/dot_sketch.js
@@ -0,0 +1,42 @@
+// JavaScript Document
+function setup() {
+  //createCanvas(400,400);
+	createCanvas(displayWidth, displayHeight);
+  //frameRate(10);
+  noLoop(); //important when saving an image at the end
+}
+
+function draw() {
+  background(255,128,128, 128);
+  
+  
+  var n = 300; //number of times loop runs - also number of dots generated
+
+for (var i = 0; i < n; i++) {
+
+  //random rgb color values:
+  var r = int(random(50, 150)); 
+  var g = int(random(50, 150));
+  var b = int(random(200, 256));
+  
+  fill(r,g,b);
+    
+  
+  var x = int(random(0, 3000));
+  var y = int(random(0, 3000));
+  
+  ellipse(x, y, 10, 10);
+  
+}
+
+}
+
+function mouseMoved() {
+    fill(255,0,0);
+    ellipse(mouseX, mouseY, 20, 20);
+}
+
+function mouseClicked() {
+  clear();
+  redraw();
+}
---
+++ sketches/sketch-ocean.js
@@ -0,0 +1,39 @@
+// JavaScript Document
+
+
+function setup() {
+  createCanvas(displayWidth, 5000);
+  noLoop();
+}
+
+function draw() {
+  background(66, 194, 245);
+
+  var n = 2000;
+
+  for (var i = 0; i < n; i++) {
+
+    var r = int(random(0, 150)); //limits on range affect the color
+    var g = int(random(0, 200));
+    var b = int(random(200, 256));
+
+    fill(r, g, b, 128); //added an opacity value, 128 is 50% transparent
+
+    //random position values:
+    var x = int(random(0, 2000));
+    var y = int(random(0, 5000));
+    
+    //random size value
+    var diameter = int(random(1,3));//picks either 1 or 2
+
+
+    noStroke();
+    ellipse(x, y, diameter*10); //draws a circle - diameter is either 10 or 20
+
+  }
+
+}
+
+function mouseClicked() {
+  redraw();
+}
